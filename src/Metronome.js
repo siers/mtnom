a2270eed62f5b5ab3e808a5e53e91ba4469ee5e6
siers <wimuan@gmail.com>
2017-09-04 18:28:20 +0300

    Bare bones.

---
+++ src/Metronome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Metronome.css'
+
+class Metronome extends Component {
+  render() {
+    return (
+      <div className="metronome">
+      </div>
+    )
+  }
+}
+
+export default Metronome
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Metronome from './Metronome'
+
+ReactDOM.render(<Metronome />, document.getElementById('root'))
