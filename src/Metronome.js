796a54efe1b9710393bbd7cb5351a30b42f22f51
siers <wimuan@gmail.com>
2017-09-07 15:31:40 +0300

    Make the button work.

--- src/Metronome.js
+++ src/Metronome.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import './Metronome.css'
+import { addRow } from './actions'
 
 class Metronome extends Component {
   render() {
     return (
       <div id="metronome">
-        <a className="add-row" />
+        <a className="add-row" onClick={() => this.props.addRow()} />
       </div>
     )
   }
@@ -17,4 +18,8 @@ const mapStateToProps = state => ({
   on: state.on
 })
 
-export default connect(mapStateToProps)(Metronome)
+const mapDispatchToProps = {
+  addRow
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Metronome)
